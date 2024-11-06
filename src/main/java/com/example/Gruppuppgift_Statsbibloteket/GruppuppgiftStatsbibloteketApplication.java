ce193a461dcacc1e4d603998aa76163608580860
Skarader <lukasholmwolf@gmail.com>
2024-11-06 10:35:25 +0100

    first commit

---
+++ src/main/java/com/example/Gruppuppgift_Statsbibloteket/GruppuppgiftStatsbibloteketApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gruppuppgift_Statsbibloteket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppuppgiftStatsbibloteketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppuppgiftStatsbibloteketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gruppuppgift_Statsbibloteket/GruppuppgiftStatsbibloteketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gruppuppgift_Statsbibloteket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppuppgiftStatsbibloteketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
