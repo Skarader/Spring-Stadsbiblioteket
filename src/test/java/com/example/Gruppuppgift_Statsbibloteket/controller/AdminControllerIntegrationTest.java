8a5d284c5d202ecc6a33e94ffb850c5057ec54b7
EddieMollerstrom <eddiemollerstrom123@gmail.com>
2024-11-19 12:01:43 +0100

    added a integrationtest for admincontroller

---
+++ src/test/java/com/example/Gruppuppgift_Statsbibloteket/controller/AdminControllerIntegrationTest.java
@@ -0,0 +1,78 @@
+package com.example.Gruppuppgift_Statsbibloteket.controller;
+
+import com.example.Gruppuppgift_Statsbibloteket.Dto.AdminAddBookDTO;
+import com.example.Gruppuppgift_Statsbibloteket.Dto.BookDTO;
+import com.example.Gruppuppgift_Statsbibloteket.model.Admins;
+import com.example.Gruppuppgift_Statsbibloteket.repository.AdminsRepository;
+import com.example.Gruppuppgift_Statsbibloteket.repository.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdminControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AdminsRepository adminsRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+        Admins admin = new Admins();
+        admin.setRole("ADMIN");
+        admin.setUsername("eddie");
+        admin.setPassword("abc123");
+        adminsRepository.save(admin);
+    }
+
+    @Test
+    void createBookIfAdminIsPresent() throws Exception {
+        AdminAddBookDTO adminAddBookDTO = new AdminAddBookDTO();
+
+        adminAddBookDTO.setNewBook(new BookDTO());
+        adminAddBookDTO.setUsername("eddie");
+        adminAddBookDTO.setPassword("abc123");
+
+        ResultActions response = mockMvc.perform(post("/admins/create/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                        "username": "eddie",
+                        "password": "abc123",
+                          "newBook": {
+                            "title": "Test Book",
+                            "publicationYear": 2024,
+                            "authorId": 2,
+                            "available": true,
+                            "bookGenreIds": [2]
+                          }
+                        }
+                        """));
+
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("Test Book")).hasJsonPath())
+                .andExpect(jsonPath("$.publicationYear", is(2024)).hasJsonPath())
+                .andDo(print());
+    }
+
+
+
+}
